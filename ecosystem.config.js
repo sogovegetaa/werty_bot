047f9f7982cf7cd9fda16bf2f266969905d9d5f9
kuanysh <kmustakh@werty.tech>
2025-11-12 22:08:39 +0500

    added ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+      {
+        name: "kesha-bot",
+        script: "dist/index.js",
+        env: {
+          NODE_ENV: "production",
+          PUPPETEER_EXECUTABLE_PATH: "/snap/bin/chromium",
+          PUPPETEER_CACHE_DIR: "/dev/null",
+          PUPPETEER_SKIP_DOWNLOAD: "true",
+          LD_LIBRARY_PATH: "/usr/lib/x86_64-linux-gnu",
+        },
+      },
+    ],
+  };
+  
